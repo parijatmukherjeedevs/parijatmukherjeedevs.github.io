86972e4f0bf38e0447ba65f6f3da79d8492ddbd3
parijatmukherjeedevs <130067218+parijatmukherjeedevs@users.noreply.github.com>
2023-04-07 12:59:42 +0530

    Update DisplayBox.jsx

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -12,7 +12,7 @@ const textAreaStyle={
 
 function DisplayBox(props){
     return(
-        <textarea class="form-control" id={props.id} rows="15" cols="60" placeholder={props.placeholder} style={textAreaStyle}></textarea> 
+        <textarea class="form-control" id={props.id} rows="5" cols="10" placeholder={props.placeholder} style={textAreaStyle}></textarea> 
 
     );
 };
